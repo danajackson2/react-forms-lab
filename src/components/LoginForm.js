2698a28d1dc38dbfd21be0b0aad6fcf92d76d26c
danajackson2 <danajackson2@gmail.com>
2021-02-03 11:36:42 -0800

    i got the power

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,40 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+  handleUserData = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    if(this.state.username !== "" && this.state.password !== ""){
+      this.props.handleLogin(this.state)
+    } else {
+      console.log('Fill in both fields')
+    }
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" value={this.state.username} onChange={this.handleUserData}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" value={this.state.password} onChange={this.handleUserData}/>
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -3,15 +3,26 @@ import React from "react";
 class TwitterMessage extends React.Component {
   constructor() {
     super();
+    this.state = {
+      tweet: "",
+      charRemain: 280
+    };
+  }
 
-    this.state = {};
+  handleChange = (event) => {
+    let numLeft = 280 - event.target.value.length
+    this.setState({
+      tweet: event.target.value,
+      charRemain: numLeft
+    })
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" onChange={this.handleChange} value={this.state.tweet}/><br />
+        <span name="char-remain">Only {this.state.charRemain} characters left you verbose piglet!</span>
       </div>
     );
   }
